ac1c1053df9f2966c2af6a20d0c39c7cff5c7785
super-cooper <adamcooperrc@gmail.com>
2017-12-08 16:40:05 -0500

    December 8th solutions

--- December08/reg1.py
+++ December08/reg1.py
@@ -1,21 +1,13 @@
 from sys import stdin
 insts = [line.strip().split() for line in stdin.readlines()]
 regs = {}
-funcs = {
-    'inc': (lambda reg, amt, how, cmp: amt if funcs[how](regs[reg], cmp) else 0),
-    'dec': (lambda reg, amt, how, cmp: -funcs['inc'](reg, amt, how, cmp)),
-    '<':   (lambda x, y: x < y),
-    '>':   (lambda x, y: x > y),
-    '==':  (lambda x, y: x == y),
-    '<=':  (lambda x, y: x <= y),
-    '>=':  (lambda x, y: x >= y),
-    '!=':  (lambda x, y: x != y)
-}
 for inst in insts:
     mod, cmd, num, oth, boo, chk = inst[0], inst[1], int(inst[2]), inst[4], inst[5], int(inst[6])
     if mod not in regs:
         regs[mod] = 0
     if oth not in regs:
         regs[oth] = 0
-    regs[mod] += funcs[cmd](oth, num, boo, chk)
+    do = False
+    exec('do = {} {} {}'.format(regs[oth], boo, chk))
+    regs[mod] += (num if cmd == 'inc' else -num) if do else 0
 print(regs[max(regs, key=lambda x: regs[x])])
