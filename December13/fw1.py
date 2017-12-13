cda3c58bbb0322e2aa1986622ebf4b5e50e7594d
super-cooper <adamcooperrc@gmail.com>
2017-12-13 16:57:55 -0500

    December 13th solutions

--- December13/fw1.py
+++ December13/fw1.py
@@ -1,3 +1,3 @@
 from sys import stdin
-print(sum(layer[0] * layer[1] if ((layer[0] + 1) % (layer[1] - 1) == 1) else 0 for layer in
+print(sum(l[0] * l[1] if ((l[0] + 1) % (l[1] - 1) == 1) else 0 for l in
           [tuple(int(v) for v in l.split(': ')) for l in stdin.readlines()]))
