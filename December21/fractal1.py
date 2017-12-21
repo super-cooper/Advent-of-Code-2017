74a8f95f3c69bfc8b025619c7f66e39835c0a3b5
super-cooper <adamcooperrc@gmail.com>
2017-12-21 06:53:41 -0500

    December 21st solutions

---
+++ December21/fractal1.py
@@ -0,0 +1,28 @@
+from sys import stdin; from copy import deepcopy
+rules = {tuple(r[:r.find(' ')].split('/')): r[r.find('=> ') + 3:].strip().split('/') for r in
+         [line.strip() for line in stdin.readlines()]}
+for rule in list(rules.keys()):
+    r = deepcopy(rule)
+    for _ in range(4):
+        rules[tuple(v[::-1] for v in r)] = rules[rule]
+        r = tuple(''.join(v) for v in zip(*r[::-1]))
+        rules[r] = rules[rule]
+img = [".#.", "..#", "###"]
+for _ in range(5):
+    enhanced = []
+    div = 2 if len(img[0]) % 2 == 0 else 3
+    for x in range(len(img[0]) // div):
+        for y in range(len(img) // div):
+            p = []
+            for yi in range(y * div, (y * div) + div):
+                sub = ''
+                for xi in range(x * div, (x * div) + div):
+                    sub += img[yi][xi]
+                p.append(sub)
+            p = tuple(p)
+            for i, row in enumerate(rules[p]):
+                if y * len(rules[p]) + i >= len(enhanced):
+                    enhanced.append('')
+                enhanced[y * len(rules[tuple(p)]) + i] = enhanced[y * len(rules[p]) + i] + row
+    img = enhanced
+print(sum(s.count('#') for s in img))
